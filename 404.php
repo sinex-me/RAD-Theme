5186b132f5e8d8e28e2d36e8b8831f6b3c65a5f3
sinex.me <sinex.me@gmail.com>
2021-05-13 17:02:41 +0300

    final2

--- 404.php
+++ 404.php
@@ -1,24 +1,11 @@
+<main> <h1>page not found, sorry</h1> </main>
 
-    <?php
-	
-	get_header();
-	
-	?>
+<h1>Reccent Posts</h1>
+<?php wp_get_archives(array('type' => 'postbypost', 'limit'
+=> 10, 'format' =>
+ 'custom','before' =>'', 'after'
+=> '<br />')); ?>
 
-		<article class="content px-3 py-5 p-md-5">
-
-		<h1>Page Not Found Sorry!</h1>
-
-       // <!-- <?php
-
-       // get_search_form();
-
-      //  ?> -->
-	    </article>
-	    
-
-	<?php
-	get_footer();
-
-	?>
-
+<?php
+get_footer();
+?>
