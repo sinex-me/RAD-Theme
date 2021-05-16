d80c1efa9fe4f85124e18ff036314e2b0b64d453
benyam <benyamz01@gmail.com>
2021-05-16 17:45:23 +0300

    altering the page

--- page.php
+++ page.php
@@ -2,7 +2,7 @@
     <?php
 	
 	get_header();// function
-	
+	//Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
 	?>
 
 		<article class="content px-3 py-5 p-md-5">
